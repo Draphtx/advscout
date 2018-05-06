b2eeede50b2b962148b9f675e2b3c62529ca648d
D <draphtx@gmail.com>
2018-05-06 00:55:15 -0400

    Basic working model of grooming route points down based on search radius

--- scratchpad/gps_sandbox.py
+++ scratchpad/gps_sandbox.py
@@ -6,6 +6,9 @@ gps_rawdata = open('Piedmont-ADV-Routes.gpx', 'r')
 
 gpx = gpxpy.parse(gps_rawdata)
 
+# Manually specifying a search radius for a POC
+radius = 1000  # feet
+
 print gpx
 
 
@@ -18,21 +21,47 @@ def avg_pd(points):
     return sum(pds)/len(pds)
 
 
-def groom_points(points, pd_avg):
-    return [item for index, item in enumerate(points[:-1]) if Haversine([item.latitude, item.longitude], [points[index+1].latitude, points[index+1].longitude]).feet < pd_avg]
+def groom_points(points, search_radius):
+    groomed_points = []
 
+    avg_pds = avg_pd(points)
+    pointer = 0
+    active_point = points[pointer]
+
+    while True:
+        try:
+            pointer += 1
+            if not (Haversine([active_point.latitude, active_point.longitude], [points[pointer].latitude, points[pointer].longitude]).feet > (search_radius - avg_pds)):
+                # print "Discarding point {0}".format(points[pointer].name)
+                pass
+            else:
+                # print "Accepting point {0}".format(active_point.name)
+                groomed_points.append(active_point)
+                active_point = points[pointer - 1]
+        except IndexError:
+            # print "Accepting point {0}".format(points[pointer-1].name)
+            groomed_points.append(points[pointer - 1])
+            return groomed_points
 
 
 def main():
-    for route in gpx.routes:
+    for index, route in enumerate(gpx.routes):
         print "Analyzing Route: {name}".format(name=route.name)
 
         pd_avg = avg_pd(route.points)
         print "Average distance between points: {pd} feet".format(pd=pd_avg)
 
-        groomed_points = groom_points(route.points, pd_avg)
+        groomed_points = groom_points(route.points, radius)
 
         print "Groomed points to {0} from {1}".format(len(groomed_points), len(route.points))
+
+        pd_avg = avg_pd(groomed_points)
+        print "Average distance between points: {pd} feet".format(pd=pd_avg)
+
+        gpx.routes[index].points = groomed_points
+        print len(gpx.routes[index].points)
+        print()
+
 
 if __name__ == '__main__':
     main()
